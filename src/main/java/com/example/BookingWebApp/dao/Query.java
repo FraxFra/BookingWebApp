e74112452ba8b5af989826dbf51cb67d4b5c65a5
Francesco <romano.fra20@outlook.it>
2023-02-14 13:39:53 +0100

    Case Sensitive Fix

--- src/main/java/com/example/BookingWebApp/dao/Query.java
+++ src/main/java/com/example/BookingWebApp/dao/Query.java
@@ -2,7 +2,7 @@ package com.example.BookingWebApp.dao;
 
 public class Query {
     public static final String getAvailableBookings = "SELECT *\n" +
-            "  FROM AvailableBooking\n" +
+            "  FROM availablebooking\n" +
             " WHERE (SubjectName = ? or ? ='')\n" +
             "   AND (TeacherId = ? or ? ='')";
 
@@ -11,12 +11,12 @@ public class Query {
             "       T.Id,\n" +
             "       ?,\n" +
             "       'Booked'\n" +
-            "FROM Teaching T\n" +
+            "FROM teaching T\n" +
             "WHERE T.TeacherId = ?\n" +
             "  AND T.SubjectId = ? ;";
 
     public static final String checkSlot = "SELECT count(*) as num" +
-            " FROM Booking B " +
+            " FROM booking B " +
             "WHERE B.SlotId = ?" +
             "  AND B.UserId = ?" +
             "  AND B.BookingStatus = 'Booked'";
@@ -26,7 +26,7 @@ public class Query {
             "       Name,\n" +
             "       Surname,\n" +
             "       Role\n" +
-            "  FROM Users\n" +
+            "  FROM users\n" +
             " WHERE Username = ?\n" +
             "     AND Password = PASSWORD(?);";
     public static final String signup = "INSERT INTO Users(Username,Password,Name,Surname,Role)\n" +
@@ -36,16 +36,16 @@ public class Query {
             "       ?,\n" +
             "       FALSE;";
     public static final String checkUser = "SELECT count(*) AS num \n" +
-            "              FROM Users U\n " +
+            "              FROM users U\n " +
             "             WHERE U.Username = ?;";
 
     public static final String getTeacher = "SELECT Id as Value,\n" +
             "       CONCAT(Name,' ',Surname) as Description\n" +
-            "  FROM Teacher\n" +
+            "  FROM teacher\n" +
             " WHERE Disabled = false;";
     public static final String getSubject = "SELECT Name as Value,\n" +
             "       Name as Description\n" +
-            "  FROM Subject" +
+            "  FROM subject" +
             " WHERE Disabled = false;";
 
     public static final String getOwnBookings = "SELECT D.Name  AS TeacherName,\n" +
@@ -64,13 +64,13 @@ public class Query {
             "       T.Id AS SlotId,\n" +
             "       B.Id                         AS BookingId,\n" +
             "       B.UserId              \n"+
-            "  FROM Slot T\n" +
-            " CROSS JOIN Teaching M\n" +
-            "       JOIN Teacher D ON M.TeacherId = D.Id\n" +
-            "       JOIN Subject S ON M.SubjectId = S.Name\n" +
-            "       JOIN Booking B ON B.SlotId = T.Id\n" +
+            "  FROM slot T\n" +
+            " CROSS JOIN teaching M\n" +
+            "       JOIN teacher D ON M.TeacherId = D.Id\n" +
+            "       JOIN subject S ON M.SubjectId = S.Name\n" +
+            "       JOIN booking B ON B.SlotId = T.Id\n" +
             "                     AND B.TeachingId = M.Id\n" +
-            "  WHERE (B.UserId = ? or EXISTS(SELECT * FROM Users WHERE Username = ? AND Role = true))\n" +
+            "  WHERE (B.UserId = ? or EXISTS(SELECT * FROM users WHERE Username = ? AND Role = true))\n" +
             "    AND B.BookingStatus = 'Booked'\n" +
             "    AND M.Disabled = false\n" +
             "    AND D.Disabled = false\n" +
@@ -78,8 +78,8 @@ public class Query {
             "    AND (D.Id= ? or ?='');";
 
     public static final String checkBooking = "SELECT count(*) AS num\n" +
-            "  FROM Booking B\n" +
-            " WHERE (B.UserId = ? or EXISTS(SELECT * FROM Users WHERE Username = ? AND Role = true))\n" +
+            "  FROM booking B\n" +
+            " WHERE (B.UserId = ? or EXISTS(SELECT * FROM users WHERE Username = ? AND Role = true))\n" +
             "   AND B.Id = ?;";
     public static final String cancelBooking = "UPDATE Booking\n" +
             "SET BookingStatus = 'Canceled'\n" +
@@ -104,10 +104,10 @@ public class Query {
             "           END AS WeekDate,\n" +
             "       B.Id AS BookingId,\n" +
             "       B.UserId AS UserId\n" +
-            "FROM Booking B\n" +
-            "         JOIN Teaching G ON B.TeachingId = G.Id\n" +
-            "         JOIN Teacher T ON G.TeacherId = T.Id\n" +
-            "         JOIN Slot S ON B.SlotId = S.Id\n" +
+            "FROM booking B\n" +
+            "         JOIN teaching G ON B.TeachingId = G.Id\n" +
+            "         JOIN teacher T ON G.TeacherId = T.Id\n" +
+            "         JOIN slot S ON B.SlotId = S.Id\n" +
             "WHERE (B.UserId = ? or ? ='')\n" +
             "  AND (T.Id = ? or ? ='')\n" +
             "  AND (G.SubjectId = ? or ? ='')\n" +
@@ -119,24 +119,24 @@ public class Query {
             "  FROM users;";
 
     public static final String getTeacherList = "SELECT id, Name, Surname FROM teacher WHERE Disabled = false;";
-    public static final String deleteTeacher= "Update Teacher set Disabled= 1 WHERE id= ? ";
-    public static final String addTeacher = "Insert into Teacher (Name,Surname)values(?,?)";
-    public static final String checkTeacher = "SELECT sum(Disabled) as num FROM Teacher WHERE Name = ? and Surname = ?; ";
-    public static final String enableTeacher = "UPDATE Teacher set Disabled = 0 WHERE Name = ? and Surname = ?";
+    public static final String deleteTeacher= "Update teacher set Disabled= 1 WHERE id= ? ";
+    public static final String addTeacher = "Insert into teacher (Name,Surname)values(?,?)";
+    public static final String checkTeacher = "SELECT sum(Disabled) as num FROM teacher WHERE Name = ? and Surname = ?; ";
+    public static final String enableTeacher = "UPDATE teacher set Disabled = 0 WHERE Name = ? and Surname = ?";
 
-    public static final String addSubject = "Insert into Subject(Name)values(?)";
-    public static final String deleteSubject= "Update Subject set Disabled=1 WHERE Name= ? ";
-    public static final String checkSubject = "SELECT sum(Disabled) as num FROM `Subject` WHERE Name = ? ";
-    public static final String enableSubject = "Update Subject set Disabled=0 WHERE Name= ? ";
+    public static final String addSubject = "Insert into subject(Name)values(?)";
+    public static final String deleteSubject= "Update subject set Disabled=1 WHERE Name= ? ";
+    public static final String checkSubject = "SELECT sum(Disabled) as num FROM `subject` WHERE Name = ? ";
+    public static final String enableSubject = "Update subject set Disabled=0 WHERE Name= ? ";
 
     public static final String getTeachings= "SELECT SubjectId, t.Id, concat(a.Name,' ',a.Surname)as TeacherName, a.Id as TeacherId  \n" +
             "from teaching t \n" +
             "join teacher a on t.TeacherId = a.Id \n" +
             "join subject s on t.SubjectId = s.Name\n" +
             "WHERE t.Disabled = 0 and a.Disabled=0 and s.Disabled=0 and (s.Name= ? or ?='') and (a.Id= ? or ?='');  ";
-    public static final String addTeachings= "Insert into Teaching(TeacherId,SubjectId)values(?,?);";
-    public static final String deleteTeachings= "Update Teaching set Disabled= 1 WHERE id= ?";
-    public static final String enableTeaching= "Update Teaching set Disabled=0 where TeacherId=? and SubjectId=?;";
-    public static final String checkTeaching="SELECT sum(Disabled) as num FROM `Teaching` WHERE  TeacherId=? and SubjectId=?";
+    public static final String addTeachings= "Insert into teaching(TeacherId,SubjectId)values(?,?);";
+    public static final String deleteTeachings= "Update teaching set Disabled= 1 WHERE id= ?";
+    public static final String enableTeaching= "Update teaching set Disabled=0 where TeacherId=? and SubjectId=?;";
+    public static final String checkTeaching="SELECT sum(Disabled) as num FROM `teaching` WHERE  TeacherId=? and SubjectId=?";
 }
 
