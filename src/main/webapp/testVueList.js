622e9d67dd185718f44a0f14bd84b916cc09037f
Striker928 <simostriker@gmail.com>
2023-03-17 14:53:59 +0100

    Fix vari alla lista e test per il filtro a checkbox

---
+++ src/main/webapp/testVueList.js
@@ -0,0 +1,56 @@
+var vm = new Vue({
+    data: {
+        materie: [],
+        professori: [],
+        selectedSubjects: [],
+        selectedProfessors: [],
+    },
+    el: '#list',
+    mounted (){
+        this.loadSubjects();
+        this.loadProfessors();
+    },
+    methods: {
+        loadSubjects() {
+            $.ajax({
+                url: "SlotServlet",
+                data: {
+                    operation: "subjectDDL"
+                },
+                method: "POST",
+                success: function(result) {
+                    if (result.ok) {
+                        vm.materie = result.data.map(name => {
+                            return{
+                                name:name.description
+                            }
+                        });
+                    } else {
+                        // vm.openError(result.error);
+                    }
+                }
+            });
+        },
+        loadProfessors() {
+            $.ajax({
+                url: "SlotServlet",
+                data: {
+                    operation: "teacherDDL"
+                },
+                method: "POST",
+                success: function(result) {
+                    if (result.ok) {
+                        vm.professori = result.data.map(name => {
+                            return{
+                                name:name.description
+                            }
+                        });
+                    } else {
+                        // vm.openError(result.error);
+                    }
+                }
+            });
+        },
+    }
+
+});
